ecd84d6ddcd5a7c334253f04fe9f9a162d9b59df
qianyiwangmeng <1780311771@qq.com>
2023-10-13 22:39:40 +0800

    finish exercise

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,13 +3,12 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+// 每个之间用；隔开
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
     }
